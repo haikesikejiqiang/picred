5cdb019a4addb90de371e796af1af0330d0021fe
haikesikejiqiang <54676348+haikesikejiqiang@users.noreply.github.com>
2020-06-01 22:22:39 +0800

    Delete banner-1.js

--- 2020/03/13/banner-1.js
+++
@@ -1,5 +0,0 @@
-var full_page = document.getElementsByClassName("full_page");
-if (full_page.length != 0) {
-  full_page[0].style.background = "transparent";
-  [data-theme='dark'] #nav, [data-theme='dark'] .layout_post > #post, [data-theme='dark'] .layout_page > div:first-child:not(.recent-posts).style.background = "transparent";	
-}
