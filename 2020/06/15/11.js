445d46ad7859a76b7602cea334308037262a6e55
haikesikejiqiang <54676348+haikesikejiqiang@users.noreply.github.com>
2020-06-15 22:01:41 +0800

    Add files via upload

---
+++ 2020/06/15/11.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $(".flink").parent().parent().addClass("add-layout")
+    $(".flink").parent().addClass("add-page")
+    $(".flink").parent().next().addClass("hide-aside")
+})
